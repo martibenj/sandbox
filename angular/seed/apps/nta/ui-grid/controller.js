190885e6de08f998427935a968c5865e7375dd80
martiben <dfgd.dfgd@dfg.com>
2016-05-31 13:44:38 +0200

    Seed : Displaying data added to ui-grid

--- angular/seed/apps/nta/ui-grid/controller.js
+++ angular/seed/apps/nta/ui-grid/controller.js
@@ -1,57 +1,72 @@
 // main.js
-var app = angular.module('myApp', ['ui.grid']);
+var app = angular.module('myApp', [ 'ui.grid' ]);
 app.controller('MyCtrl', function($scope) {
 
-    $scope.title = "POC NTA avec composant ng-grid";
-    
-    $scope.myDataInit = [{
-        idFT: "FT1",
-        listLFT: [{ idLFT: 'LFT1-de-la-FT1', descLFT: 'descLFT1' }, { idLFT: 'LFT2-de-la-FT1', descLFT: 'descLFT2' }]
-    }, {
-        idFT: "FT2",
-        listLFT: [{ idLFT: 'LFT1-de-la-FT2', descLFT: 'descLFT1' }, { idLFT: 'LFT2-de-la-FT2', descLFT: 'descLFT2' }]
-    }];
-    console.log('myDataInit:' + $scope.myDataInit);
-    // Lecture de la liste des infos des objets FT et LFT
-    //angular.forEach($scope.myData, function(value, key, item) {
-    //console.log('avantValue:' + value);
-    //console.log('avantKey:' + key);
-    //console.log('avantItem:' + item);
-    //console.log('avantValue.idFT:' + value.idFT);
-    //console.log('avantValue.listLFT:' + value.listLFT);
-    // angular.forEach(value.listLFT, function(value, key, item) {
-    //console.log('dedansValue.idLFT:' + value.idLFT);
-    //console.log('dedansValue.descLFT:' + value.descLFT);
-    //});
-    //});
+	$scope.title = "POC NTA avec composant ui-grid";
 
-    $scope.gridOptions = {
-        data: 'myDataTraitee',
-        multiSelect: false,
-        columnDefs: [{ field: 'idFT', displayName: 'Num FT' },
-            { field: 'idLFT', displayName: 'Num LFT' }
-        ]
-    };
+	$scope.myDataInit = [ {
+		idFT : "FT1",
+		listLFT : [ {
+			idLFT : 'LFT1-de-la-FT1',
+			descLFT : 'descLFT1'
+		}, {
+			idLFT : 'LFT2-de-la-FT1',
+			descLFT : 'descLFT2'
+		} ]
+	}, {
+		idFT : "FT2",
+		listLFT : [ {
+			idLFT : 'LFT1-de-la-FT2',
+			descLFT : 'descLFT1'
+		}, {
+			idLFT : 'LFT2-de-la-FT2',
+			descLFT : 'descLFT2'
+		} ]
+	} ];
 
-    $scope.myDataTraitee = [];
-    console.log('myDataTraitee:' + $scope.myDataTraitee);
+	console.log('myDataInit:' + $scope.myDataInit);
+	// Lecture de la liste des infos des objets FT et LFT
+	// angular.forEach($scope.myData, function(value, key, item) {
+	// console.log('avantValue:' + value);
+	// console.log('avantKey:' + key);
+	// console.log('avantItem:' + item);
+	// console.log('avantValue.idFT:' + value.idFT);
+	// console.log('avantValue.listLFT:' + value.listLFT);
+	// angular.forEach(value.listLFT, function(value, key, item) {
+	// console.log('dedansValue.idLFT:' + value.idLFT);
+	// console.log('dedansValue.descLFT:' + value.descLFT);
+	// });
+	//	});
 
-    angular.forEach($scope.myDataInit, function(itemOfTable) {
-        console.log('itemOfTable.listLFT:' + itemOfTable.listLFT);
-        var nomsLFT = '';
-        angular.forEach(itemOfTable.listLFT, function(itemLFT, numRang) {
-            console.log('itemLFT.idLFT:' + itemLFT.idLFT);
-            if (numRang != 0) {
-                nomsLFT += " et "
-            }
-            $scope.myDataTraitee.push({
-                idFT: itemOfTable.idFT,
-                idLFT: itemLFT.idLFT
-            });
-        });
-    });
+	$scope.gridOptions = {
+		data : 'myDataTraitee',
+		multiSelect : false,
+		columnDefs : [ {
+			field : 'idFT',
+			displayName : 'Num FT'
+		}, {
+			field : 'idLFT',
+			displayName : 'Num LFT'
+		} ]
+	};
 
-    console.log('myDataTraitee:' + $scope.myDataTraitee);
+	$scope.myDataTraitee = [];
 
+	angular.forEach($scope.myDataInit, function(itemOfTable) {
+		console.log('itemOfTable.listLFT:' + itemOfTable.listLFT);
+		var nomsLFT = '';
+		angular.forEach(itemOfTable.listLFT, function(itemLFT, numRang) {
+			console.log('itemLFT.idLFT:' + itemLFT.idLFT);
+			if (numRang != 0) {
+				nomsLFT += " et "
+			}
+			$scope.myDataTraitee.push({
+				idFT : itemOfTable.idFT,
+				idLFT : itemLFT.idLFT
+			});
+		});
+	});
+
+	console.log('myDataTraitee:' + $scope.myDataTraitee);
 
 });
